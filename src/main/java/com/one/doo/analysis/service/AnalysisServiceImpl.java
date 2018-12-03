e9e99f60a7535c82153b09d0094e09e02cac390a
DESKTOP-O0K1JML\kosta <paskal1234@naver.com>
2018-12-03 17:45:04 +0900

    분석 페이지 유동적인 동 선택을 위한 ajax 통신

--- src/main/java/com/one/doo/analysis/service/AnalysisServiceImpl.java
+++ src/main/java/com/one/doo/analysis/service/AnalysisServiceImpl.java
@@ -4,19 +4,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.one.doo.board.domain.Criteria;
-import com.one.doo.member.domain.Auth;
-import com.one.doo.member.domain.Member;
-import com.one.doo.member.mapper.AuthMapper;
-import com.one.doo.member.mapper.MemberMapper;
-import com.one.doo.member.validate.MailCertify;
-import com.one.doo.metadata.log.domain.Log;
-import com.one.doo.metadata.log.mapper.LogMapper;
-import com.one.doo.metadata.url.mapper.UrlMapper;
+import com.one.doo.areacode.domain.AreaCode;
+import com.one.doo.areacode.mapper.AreaCodeMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -25,10 +17,26 @@ import lombok.extern.log4j.Log4j;
 @Service
 @AllArgsConstructor
 public class AnalysisServiceImpl implements AnalysisService {
+	
+	@Inject
+	AreaCodeMapper areacodeMapper;
+	
+	@Transactional
 	@Override
-	public String[] getVillageList(String selectedDistrict) {
+	public String getVillageList(String selectedDistrict) {
 		
-		return null;
+		List<AreaCode> list = areacodeMapper.getAreaCodeListByAdmin(selectedDistrict);
+		
+		String result = "";
+		
+		for (AreaCode areaCode : list) {
+			result += areaCode.getAreaCode_Name();
+			result += ",";
+		}
+		
+		result = result.substring(0, result.length() - 1);
+		
+		return result;
 	}
 
 }
