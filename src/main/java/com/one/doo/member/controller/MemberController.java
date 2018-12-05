c2b2a18670ca693df324cd5628043cb2950e55f3
DESKTOP-O0K1JML\kosta <paskal1234@naver.com>
2018-12-05 10:48:22 +0900

    hotfix : Mapper부분 수정

--- src/main/java/com/one/doo/member/controller/MemberController.java
+++ src/main/java/com/one/doo/member/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.one.doo.member.controller;
 
 import javax.inject.Inject;
 
-import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
