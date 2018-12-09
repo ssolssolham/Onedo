af4a1d0ee533ba5a12fa2aa473ddd7224048ae6d
euisoo <dmltn0828@naver.com>
2018-12-10 03:21:22 +0900

    adbd mvc, xml 완성

---
+++ src/main/java/com/one/doo/adbd/mapper/ADBDMapper.java
@@ -0,0 +1,9 @@
+package com.one.doo.adbd.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface ADBDMapper {
+
+	//게시판 글 작성
+	public void insert(@Param(value = "userId") String userid, @Param(value="alleybiz") int alleyBizCode);
+}
